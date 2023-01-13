d8e8b28d6d064af4e62d521a295aab47d725d94b
css <seop7379@daum.net>
2023-01-13 16:53:39 +0900

    spring container

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 public class SingletonTest {
     @Test
@@ -25,5 +27,18 @@ public class SingletonTest {
        SingletonService singletonService2 =  SingletonService.getInstance();
 
        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 활용한 스프링 컨테이너")
+    void springContainer() {
+        ApplicationContext applicationContext = new AnnotationConfigReactiveWebApplicationContext(AppConfig.class);
+
+        //1.조회 : 요청 시, 객체를 생성
+
+        MemberService memberService1 = applicationContext.getBean("memberService",MemberService.class);
+        MemberService memberService2 = applicationContext.getBean("memberService",MemberService.class);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
