02b4b65a3cd2d9ddae0aea63b41e3f0bed29269c
css <seop7379@daum.net>
2023-01-16 11:39:50 +0900

    Component Scan

---
+++ src/test/java/hello/core/scan/AutoConfigTest.java
@@ -0,0 +1,18 @@
+package hello.core.scan;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoConfigTest {
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext applicationContext= new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService= applicationContext.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+}
